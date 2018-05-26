07b237ae1fa1b1f009b06cb04ca3cc3ce7e3dcf9
llOldmenll <nosovmak@gmail.com>
2018-05-26 15:11:53 +0300

    - crashlytics was added

--- app/src/main/java/com/oldmen/testapplihelp/CustomApplication.java
+++ app/src/main/java/com/oldmen/testapplihelp/CustomApplication.java
@@ -3,7 +3,10 @@ package com.oldmen.testapplihelp;
 import android.app.Application;
 import android.arch.persistence.room.Room;
 
+import com.crashlytics.android.Crashlytics;
 import com.oldmen.testapplihelp.data.database.AppDataBase;
+
+import io.fabric.sdk.android.Fabric;
 
 import static com.oldmen.testapplihelp.utils.Constants.DATA_BASE_NAME;
 
@@ -13,6 +16,7 @@ public class CustomApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         mDataBase = Room.databaseBuilder(getApplicationContext(), AppDataBase.class, DATA_BASE_NAME)
                 .build();
     }
